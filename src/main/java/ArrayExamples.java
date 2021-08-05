49196cded3be4bc99b3222620030a44e9e52ae06
Bethan Palmer <bethan.palmer@digital.justice.gov.uk>
2021-08-05 08:20:20 +0100

    Add code examples for 02_02

---
+++ src/main/java/ArrayExamples.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class ArrayExamples {
+
+  public static void main(String[] args) {
+
+    String[] weekdays = new String[7];
+    System.out.println(Arrays.toString(weekdays));
+
+    weekdays[0] = "Monday";
+    System.out.println(Arrays.toString(weekdays));
+
+    weekdays = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    System.out.println(Arrays.toString(weekdays));
+
+    for(String day : weekdays) {
+      System.out.println("Week day: " + day);
+    }
+
+    for(int i = 0; i < weekdays.length; i++) {
+      weekdays[i] = weekdays[i].toUpperCase();
+    }
+
+    System.out.println(Arrays.toString(weekdays));
+
+    int[][] openingHours = new int[7][2];
+    openingHours[0][0] = 9;
+    openingHours[0][1] = 5;
+    System.out.println(Arrays.deepToString(openingHours));
+
+  }
+
+}
