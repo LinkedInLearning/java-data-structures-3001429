e4eed74da934f279dadfbb4acb54651709f0dd84
Bethan Palmer <bethan.palmer@digital.justice.gov.uk>
2021-08-07 20:37:52 +0100

    Add the arrays exercise

---
+++ src/test/java/ArrayExerciseTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayExerciseTest {
+
+  ArrayExercise arrayExercise = new ArrayExercise();
+
+  @Test
+  public void testIncrementArray() {
+    int[] input = new int[]{1, 2, 3, 4, 5};
+    arrayExercise.incrementArray(input);
+    assertArrayEquals(new int[]{2, 3, 4, 5, 6}, input);
+  }
+
+}
