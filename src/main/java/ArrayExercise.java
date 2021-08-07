862440a95059d334a54dc43445519347e222222d
Bethan Palmer <bethan.palmer@digital.justice.gov.uk>
2021-08-07 21:02:30 +0100

    Add solution for arrays exercise

--- src/main/java/ArrayExercise.java
+++ src/main/java/ArrayExercise.java
@@ -1,7 +1,9 @@
 public class ArrayExercise {
 
   public void incrementArray(int[] input) {
-    // Add code here to increment each element in the array by 1
+    for(int i = 0; i < input.length; i++) {
+      input[i] = input[i] + 1;
+    }
   }
 
 }
