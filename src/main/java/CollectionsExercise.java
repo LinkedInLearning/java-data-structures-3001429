88cf5875abbc2f7219ba428a4a02393daee06beb
Bethan Palmer <bethan.palmer@digital.justice.gov.uk>
2021-08-17 08:24:54 +0100

    Add the exercises for the Collections chapter

---
+++ src/main/java/CollectionsExercise.java
@@ -0,0 +1,28 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class CollectionsExercise {
+
+  void addToEndOfLinkedList(LinkedList<Integer> numbers, int i) {
+    numbers.add(i);
+  }
+
+  void addToStartOfLinkedList(LinkedList<Integer> numbers, int i) {
+
+  }
+
+  void removeItemFromTopOfStack(Deque<Integer> stack) {
+
+  }
+
+  void removeItemFromFrontOfQueue(Queue<Integer> queue) {
+
+  }
+
+  void addItemToTreeSet(Set<String> set, String item) {
+
+  }
+
+}
---
+++ src/test/java/CollectionsExerciseTest.java
@@ -0,0 +1,65 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
+import org.junit.jupiter.api.Test;
+
+class CollectionsExerciseTest {
+
+  CollectionsExercise collectionsExercise = new CollectionsExercise();
+
+  @Test
+  public void testAddSixToEndOfLinkedList() {
+    LinkedList<Integer> numbers = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+    collectionsExercise.addToEndOfLinkedList(numbers, 6);
+    assertEquals(Integer.valueOf(6), numbers.getLast());
+  }
+
+  @Test
+  public void testAddZeroToStartOfLinkedList() {
+    LinkedList<Integer> numbers = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+    collectionsExercise.addToStartOfLinkedList(numbers, 0);
+    assertEquals(Integer.valueOf(0), numbers.getFirst());
+  }
+
+  @Test
+  public void testRemoveItemFromTopOfStack() {
+    Deque<Integer> stack = new ArrayDeque();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    collectionsExercise.removeItemFromTopOfStack(stack);
+    assertEquals(Integer.valueOf(4), stack.peek());
+  }
+
+  @Test
+  public void testRemoveItemFromFrontOfQueue() {
+    Queue<Integer> queue = new ArrayDeque();
+    queue.offer(1);
+    queue.offer(2);
+    queue.offer(3);
+    queue.offer(4);
+    queue.offer(5);
+    collectionsExercise.removeItemFromFrontOfQueue(queue);
+    assertEquals(Integer.valueOf(2), queue.peek());
+  }
+
+  @Test
+  public void testAddItemToTreeSet() {
+    Set<String> set = new TreeSet<>();
+    set.add("red");
+    set.add("orange");
+    set.add("yellow");
+    collectionsExercise.addItemToTreeSet(set, "green");
+    assertTrue(set.contains("green"));
+  }
+
+}
