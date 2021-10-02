55a1eef06ee79b99d969ff741d77d6f962d6d129
Bethan Palmer <bethan.palmer@hotmail.com>
2021-10-02 10:44:02 +0100

    Add an example test and solution

--- src/main/java/ArrayExercise.java
+++ src/main/java/ArrayExercise.java
@@ -1,5 +1,11 @@
 public class ArrayExercise {
 
+  public void arrayToUpperCase(String[] input) {
+    for(int i = 0; i < input.length; i++) {
+      input[i] = input[i].toUpperCase();
+    }
+  }
+
   public void incrementArray(int[] input) {
     // Add code here to increment each element in the array by 1
   }
--- src/test/java/ArrayExerciseTest.java
+++ src/test/java/ArrayExerciseTest.java
@@ -7,6 +7,13 @@ class ArrayExerciseTest {
   ArrayExercise arrayExercise = new ArrayExercise();
 
   @Test
+  public void testArrayToUpperCase() {
+    String[] input = new String[]{"cat", "dog", "fish"};
+    arrayExercise.arrayToUpperCase(input);
+    assertArrayEquals(new String[]{"CAT", "DOG", "FISH"}, input);
+  }
+
+  @Test
   public void testIncrementArray() {
     int[] input = new int[]{1, 2, 3, 4, 5};
     arrayExercise.incrementArray(input);
