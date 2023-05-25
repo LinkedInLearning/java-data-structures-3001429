c2e8645d2a2c80ae11222ed232a32080edba5c2b
OkelloSam21 <samuel.okello@studentambassadors.com>
2023-05-25 11:59:19 +0300

    create array example

---
+++ src/arrayExample.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class arrayExample{
+    public static void main(String[] args){
+        String[] cars = new String[7];
+        // {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(Arrays.toString(cars));
+    }
+}
