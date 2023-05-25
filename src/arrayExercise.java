eef02be25c7c64eabd339120e2d2936e364ee35d
OkelloSam21 <samuel.okello@studentambassadors.com>
2023-05-25 13:32:10 +0300

    add an arrayToUpperCase  method inside arrayExample.java

---
+++ src/arrayExercise.java
@@ -0,0 +1,7 @@
+public class arrayExercise{
+    public void arrayToUpperCase(String[] array){
+        for(int i = 0; i < array.length; i++){
+            array[i] = array[i].toUpperCase();
+        }
+    }
+}
